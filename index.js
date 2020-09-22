e0bcaef42272f4e41ab1b4607faea7a5bf8959b0
Cat <71641613+InaneCat@users.noreply.github.com>
2020-09-22 01:02:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const Discord = require('discord.js');
-const { prefix1, prefix2, giphyToken, token } = require('./config.json');
 const client = new Discord.Client();
 const { Users, CurrencyShop } = require('./dbObjects');
 const { Op } = require('sequelize');
 const currency = new Discord.Collection();
 
-var GphApiClient = require('giphy-js-sdk-core')
-giphy = GphApiClient(giphyToken)
-
-const Eris = require("eris");
-const { Member } = require('eris');
-
-const bot = new Eris.CommandClient(`NjkxMzk4MjkzOTAwNjIzOTEy.XuasZA.QRxXObD8-VonzO6VrCB234ZLUCQ`, {}, {
-    description: "owo cat :)",
-    owner: "Inane Cat",
-    prefix: "owo "
-});
 
 Reflect.defineProperty(currency, 'add', {
 	/* eslint-disable-next-line func-name-matching */
@@ -56,60 +44,10 @@ client.on('message', message => {
   .then(console.log)
   .catch(console.error);
 
-if (!message.content.startsWith(PREFIX)) return;
-	const input = message.content.slice(PREFIX.length).trim();
-	if (!input.length) return;
-	const [, command, commandArgs] = input.match(/(\w+)\s*([\s\S]*)/);
-
-	if (command === 'balance') {
+	if (!message.content.startsWith(`${prefix1}balance`)) {
 		const target = message.mentions.users.first() || message.author;
 		return message.channel.send(`${target.tag} has ${currency.getBalance(target.id)}💰`);
-	} else if (command === 'inventory') {
-		const target = message.mentions.users.first() || message.author;
-		const user = await Users.findOne({ where: { user_id: target.id } });
-		const items = await user.getItems();
-
-		if (!items.length) return message.channel.send(`${target.tag} has nothing!`);
-		return message.channel.send(`${target.tag} currently has ${items.map(t => `${t.amount} ${t.item.name}`).join(', ')}`);
-	} else if (command === 'transfer') {
-		const currentAmount = currency.getBalance(message.author.id);
-		const transferAmount = commandArgs.split(/ +/).find(arg => !/<@!?\d+>/.test(arg));
-		const transferTarget = message.mentions.users.first();
-
-		if (!transferAmount || isNaN(transferAmount)) return message.channel.send(`Sorry ${message.author}, that's an invalid amount`);
-		if (transferAmount > currentAmount) return message.channel.send(`Sorry ${message.author} you don't have that much.`);
-		if (transferAmount <= 0) return message.channel.send(`Please enter an amount greater than zero, ${message.author}`);
-
-		currency.add(message.author.id, -transferAmount);
-		currency.add(transferTarget.id, transferAmount);
-
-		return message.channel.send(`Successfully transferred ${transferAmount}💰 to ${transferTarget.tag}. Your current balance is ${currency.getBalance(message.author.id)}💰`);
-	} else if (command === 'buy') {
-		const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: commandArgs } } });
-		if (!item) return message.channel.send('That item doesn\'t exist.');
-		if (item.cost > currency.getBalance(message.author.id)) {
-			return message.channel.send(`You don't have enough currency, ${message.author}`);
-		}
-
-		const user = await Users.findOne({ where: { user_id: message.author.id } });
-		currency.add(message.author.id, -item.cost);
-		await user.addItem(item);
-
-		message.channel.send(`You've bought a ${item.name}`);
-	} else if (command === 'shop') {
-		const items = await CurrencyShop.findAll();
-		return message.channel.send(items.map(i => `${i.name}: ${i.cost}💰`).join('\n'), { code: true });
-	} else if (command === 'leaderboard') {
-		return message.channel.send(
-			currency.sort((a, b) => b.balance - a.balance)
-				.filter(user => client.users.has(user.user_id))
-				.first(10)
-				.map((user, position) => `(${position + 1}) ${(client.users.get(user.user_id).tag)}: ${user.balance}💰`)
-				.join('\n'),
-			{ code: true }
-		);
 	}
-	
 			       
 //profile picture   
     pfpresponse = [`nice` , `bad`, `incredible` , `amazing` , `ok` , `epic`]
